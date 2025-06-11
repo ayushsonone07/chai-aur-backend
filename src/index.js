8301707d4530020cdef798ca131c68eec09516aa
ayushsonone27 <ayushsonone27@gmail.com>
2025-06-11 17:42:39 +0000

    added setup code

--- src/index.js
+++ src/index.js
@@ -5,20 +5,3 @@ dotenv.config({
     path: './env'
 });
 connectDB();
-
-// (async () => {
-//     try{
-//         await mongoose.connect(`${process.env.DATABASE_URI}/${DB_NAME}`)
-//         app.on("ERROR", (error) => {
-//             console.log("ERROR", error);
-//             throw error
-//         })
-
-//     app.listen(process.env.PORT, () => {
-//         console.log(`Listening on port ${process.env.PORT}`)
-//     })
-//     }catch(error){
-//         console.log("ERROR", error)
-//         throw error
-//     }
-// })()
